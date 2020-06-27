d2b0d8163da4759a0691a62c774ff81eb0b92010
kgaolodevs <kgaolodev@gmail.com>
2020-06-27 17:55:20 +0200

    Completed delete functionality, with updating local storage

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const actions = {
     item.innerText = elements.formInput.value;
     item.classList.add("app__list--item");
 
+    // Add item to local storage
+    actions.saveItemsLocally(item.innerText);
+
     // Attach item to list div
     listDiv.appendChild(item);
 
@@ -51,12 +54,15 @@ const actions = {
     // prettier-ignore
     if (element.classList[0] === "app__delete-Btn"){
       const parent = element.parentElement;
+      
 
       // Animation
       parent.classList.add("deleteAnimation")
       parent.addEventListener("transitionend", function(){
         parent.remove();
       })
+
+      actions.deleteLocalItem(parent.textContent)
     }
   },
   completedItem: function (e) {
@@ -91,12 +97,66 @@ const actions = {
   saveItemsLocally: function (item) {
     // Check if local storage has existing items
     let allItems;
-    if (localStorage.getItem("allItems") === null) {
+    if (localStorage.getItem("To do") === null) {
       allItems = [];
     } else {
-      allItems = JSON.parse(localStorage.getItem("allItems"));
+      allItems = JSON.parse(localStorage.getItem("To do"));
     }
+
     allItems.push(item);
+
+    let itemsData = JSON.stringify(allItems);
+    localStorage.setItem("To do", itemsData);
+  },
+  retrieveLocalItems: function (e) {
+    let allItems;
+    if (localStorage.getItem("To do") === null) {
+      allItems = [];
+    } else {
+      allItems = JSON.parse(localStorage.getItem("To do"));
+    }
+
+    allItems.forEach(function (todo) {
+      // List Div
+      const listDiv = document.createElement("div");
+      listDiv.classList.add("app__item");
+
+      // Create list item
+      const item = document.createElement("li");
+      item.innerText = todo;
+      item.classList.add("app__list--item");
+
+      // Attach item to list div
+      listDiv.appendChild(item);
+
+      // Create checked button
+      const completedButton = document.createElement("button");
+      completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+      completedButton.classList.add("app__complete-Btn");
+
+      // Attach checked button to list div
+      listDiv.appendChild(completedButton);
+
+      // Create delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
+      deleteButton.classList.add("app__delete-Btn");
+
+      // Attach delete button to list div
+      listDiv.appendChild(deleteButton);
+
+      // Attach div to the app list
+      const appList = document.querySelector(".app__list");
+      appList.appendChild(listDiv);
+    });
+  },
+  deleteLocalItem: function (item) {
+    let retrievedItem = localStorage.getItem("To do");
+    let convertedItems = JSON.parse(retrievedItem);
+    let itemIndex = convertedItems.indexOf(item);
+    if (itemIndex > -1) convertedItems.splice(itemIndex, 1);
+    let newData = JSON.stringify(convertedItems);
+    localStorage.setItem("To do", newData);
   },
 };
 
@@ -105,3 +165,6 @@ elements.formButton.addEventListener("click", actions.addItem);
 elements.list.addEventListener("click", actions.deleteItem);
 elements.list.addEventListener("click", actions.completedItem);
 elements.filterItems.addEventListener("click", actions.filterItems);
+document.addEventListener("DOMContentLoaded", actions.retrieveLocalItems);
+
+localStorage.clear();
